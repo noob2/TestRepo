cf183ae4cbf64a74d06b587f6074e33b43bb33c7
viksata <viktor.jordanov.1992@gmail.com>
2017-10-29 15:20:12 +0200

    Update cars.c

--- cars.c
+++ cars.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <malloc.h>
-#include <stdlib.h>
+#include <string.h>
 
-typedef struct{
-    char* carModel;
+#define numberOfCars 4
+
+//trqbva da q napravqq s make file
+
+typedef struct {
+    char *carModel;
     int hordePower;
     int torque;
     int isTuned;
 } Car;
 
-Car * newCar(char* carModel,int hordePower, int torque, int isTuned){
-    Car * myCar = malloc(sizeof(Car));
+Car *newCar(char *carModel, int hordePower, int torque, int isTuned) {
+    Car *myCar = malloc(sizeof(Car));
     myCar->carModel = carModel;
     myCar->hordePower = hordePower;
     myCar->torque = torque;
@@ -19,53 +23,69 @@ Car * newCar(char* carModel,int hordePower, int torque, int isTuned){
     return myCar;
 }
 
-void printCar(Car * car){
-    printf("carModel: %s\n",car->carModel);
-    printf("hordePower: %d\n",car->hordePower);
-    printf("torque: %d\n",car->torque);
-    if (car->isTuned == 1){
+void printCar(Car *car) {
+    printf("carModel: %s\n", car->carModel);
+    printf("hordePower: %d\n", car->hordePower);
+    printf("torque: %d\n", car->torque);
+    if (car->isTuned == 1) {
         printf("car is tuned\n");
-    } else{
+    } else {
         printf("car is NOT tuned\n");
     }
 }
 
-Car BestTorqueCar(Car* cars){
+Car BestTorqueCar(Car *cars) {
     int index = 0;
-    for (int i = 0; i < 3; ++i) {
-        if(cars->torque > index){
+    for (int i = 0; i < numberOfCars; ++i) {
+        if (cars[i].torque > index) {
             index = i;
         }
     }
     return cars[index];
 }
 
-Car BestHPCar(Car* cars){
+Car CarByName(Car *cars, char *carName) {
+    int answer;
+    for (int i = 0; i < numberOfCars; ++i) {
+        answer = strcmp(cars[i].carModel, carName);
+        if (answer == 0) {
+            return cars[i];
+        }
+    }
+    return *newCar("null", -1, -1, -1);
+}
+
+Car BestHPCar(Car *cars) {
     int index = 0;
-    for (int i = 0; i < 3; ++i) {
-        if(cars->hordePower > index){
+    for (int i = 0; i < numberOfCars; ++i) {
+        if (cars[i].hordePower > index) {
             index = i;
         }
     }
     return cars[index];
 }
 
-int main(int argc, const char * argv[])
-{
-    Car* allCars[3] = {
-            {"alfa", 100, 30, 0}
-    };,{"alfaClocked", 110, 32, 1},{"alfaClockedVeryMuch", 110, 39, 1},{"alfaClockedVeryMuch [OVER 9000!!]", 9000, 42, 1}
+int main(int argc, const char *argv[]) {
 
-    Car bestTorqueCar = BestTorqueCar(*allCars);
-    Car bestHPCar = BestHPCar(*allCars);
 
-    printCar(allCars[0]);
-    printCar(allCars[1]);
+    Car allCars[numberOfCars] = {{"alfa",                              100,  30, 0},
+                                 {"alfaClocked",                       110,  32, 1},
+                                 {"alfaClockedVeryMuch",               110,  39, 1},
+                                 {"alfaClockedVeryMuch [OVER 9000!!]", 9000, 1,  1}};
+
+    Car bestTorqueCar = BestTorqueCar(allCars);
+    Car bestHPCar = BestHPCar(allCars);
+
+    char carName = {0};
+    scanf("%s", &carName);
+    Car carFound = CarByName(allCars, &carName);
 
     printf("best TORQUE car:\n");
     printCar(&bestTorqueCar);
     printf("best HP car:\n");
     printCar(&bestHPCar);
+    printf("car by name:\n");
+    printCar(&carFound);
 
     return 0;
 }
